06343dab60725337de3b465f847860c779dcb38e
Marina2022 <marusiiiia@yandex.ru>
2024-02-22 14:28:55 +0300

    first

---
+++ source/js/script.js
@@ -0,0 +1,12 @@
+const mobileBtn = document.querySelector('.mobileBtn')
+const mobileMenu = document.querySelector('.mobileMenu')
+const closeBtn = document.querySelector('.header__close')
+
+
+mobileBtn.addEventListener('click', ()=>{
+    mobileMenu.classList.remove('hidden')
+})
+
+closeBtn.addEventListener('click', ()=>{
+    mobileMenu.classList.add('hidden')
+})
